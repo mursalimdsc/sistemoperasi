c1f7451612c218514de358b8a6aa4d89b61538d9
mursalimdsc <mursalim.dsc@gmail.com>
2023-06-09 20:53:09 +0700

    Menambahkan file Latihan1 ke Repository

---
+++ latihan1.bat
@@ -0,0 +1,7 @@
+@echo off
+title Latihan 2 - Menampilkan Identitas diri
+echo NIM	            : 20220911
+echo Nama	            : MURSALIM
+echo No WA	            : 085290746061
+echo Alamat	            : Kec. Kandeman Kab Batang
+pause
